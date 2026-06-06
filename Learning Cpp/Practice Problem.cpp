7726bb727a87915bad7a1167f81a8bc08c18d177
tannuhightech27 <tannuhightechtht27@gmail.com>
2026-06-07 02:08:14 +0530

    Learning Cpp

---
+++ Learning Cpp/Practice Problem.cpp
@@ -0,0 +1,45 @@
+
+//Program for Area of Triangle
+
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+    float b,h,a;
+
+    cout<<"Enter base and height";
+    cin>>b>>h;
+
+    a= (b * h)/2;
+
+    cout<<"Area of a Triangle is "<<a;
+
+    return 0;
+}*/
+
+//Finding Sum of fisrt n natural no.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, Sum;
+
+    cout<<"Enter n";
+    cin>>n;
+
+    Sum = n*(n+1)/2;
+    cout<<"SUM is "<<Sum;
+
+    return 0;
+    
+
+}
+
+
+
+
+
+    
---
+++ Learning Cpp/main.cpp
@@ -0,0 +1,30 @@
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"Enter 2 nos";
+    cin>>a>>b;
+    c= a+b;
+    cout<<"Sum is"<<c<<endl;
+
+    return 0;
+
+} */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+
+    cout<<" May I know your name ";
+    getline(cin,str);
+
+    cout<<"Hello "<<str;
+    
+    return 0; 
+}
+
+
